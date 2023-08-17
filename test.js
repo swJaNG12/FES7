0b2de207b8131d75c39e047c6677272d546f657f
swJaNG12 <cbdjej38483@gmail.com>
2023-08-17 21:24:20 +0900

    JS update

--- test.js
+++ test.js
@@ -1,28 +1,19 @@
-let input = [
-  '11',   '1 4',  '3 5',
-  '0 6',  '5 7',  '3 8',
-  '5 9',  '6 10', '8 11',
-  '8 12', '2 13', '12 14'
-]
-
-const n = parseInt(input[0]);
-
-
-let arr = []
-for(let i = 1; i <= n; i++) {
-  arr.push(input[i].split(' ').map(Number))
+function fibonachi(n) {
+  if(n === 0) return 0;
+  if(n === 1 || n === 2) {
+      return 1;
+  }
+  return fibonachi(n-2) + fibonachi(n-1);
 }
-arr.sort((a,b) => a[1] - b[1])
+
+let input = 100;
+let arr =  [];
+let n = 0
 
 
-let answer = [arr[0]]
-let index = 0
-
-for(let i = 1; i < arr.length; i++) {
-  let next = arr.filter(el => arr[index][1] <= el[0])[0]
-  index = arr.findIndex(arr => {
-    return JSON.stringify(arr) === JSON.stringify(next)
-  })
-  answer.push(next)
+while(input > 1) {
+  while(fibonachi(++n) <= input) {}
+  arr.push(fibonachi(n-1))
+  input -= fibonachi(n-1)
+  n = 0;
 }
-console.log(answer)
